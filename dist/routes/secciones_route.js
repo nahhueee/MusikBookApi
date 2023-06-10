d894caab086c08f715049fcb7dc865925d7adf17
nahhueee <nahu852na@gmail.com>
2023-06-10 13:19:00 -0300

    Primer commit desde secundaria

--- dist/routes/secciones_route.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const secciones_control_1 = require("../controllers/secciones_control");
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-router.post('/agregar', secciones_control_1.seccionesctrl.Agregar);
-router.put('/modificar', secciones_control_1.seccionesctrl.Modificar);
-// Export the router
-exports.default = router;
--- dist/routes/tipoCancion_route.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tipoCancion_control_1 = require("../controllers/tipoCancion_control");
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-router.get('/select', tipoCancion_control_1.tipoCancionctrl.ObtenerTipoCancionSelector);
-// Export the router
-exports.default = router;
